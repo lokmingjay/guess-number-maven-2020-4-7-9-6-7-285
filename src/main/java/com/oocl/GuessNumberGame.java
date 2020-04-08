c212a3c71c9dc3b72165eaa900179e6e3e787ed3
JAY NGAN (DEV-ISD-OOCLL/HKG) <NGANJA@OOCL.COM>
2020-04-08 20:29:17 +0800

    [NGANJA] add should get same number between input and random number

--- src/main/java/com/oocl/GuessNumberGame.java
+++ src/main/java/com/oocl/GuessNumberGame.java
@@ -3,16 +3,24 @@ package com.oocl;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class GuessNumberGame {
     private static final int RANDOM_DIGIT = 4;
+
     private String randomAnswer;
 
 
     public String getRandomAnswer() {
         return randomAnswer;
     }
+
+    public void setRandomAnswer(String randomAnswer) {
+        this.randomAnswer = randomAnswer;
+    }
+
 
     public GuessNumberGame() {
         randomAnswer = randomNumberGenerator();
@@ -28,4 +36,18 @@ public class GuessNumberGame {
         return randomNumberString;
     }
 
+   // public String play(String input) {
+        // first get the common element from the input where value is same as random value
+        // e.g. 2 only, for this two ,we can compare the position between string is same
+
+       // return "";
+   // }
+
+    public List<Character> getCommonElement(String input) {
+       List<Character> inputCharList = input.chars().mapToObj(element -> (char)element).collect(Collectors.toList());
+       List<Character> randomAnsCharList = randomAnswer.chars().mapToObj(element -> (char)element).collect(Collectors.toList());
+       inputCharList.retainAll(randomAnsCharList);
+        return inputCharList;
+
+    }
 }
--- src/test/java/com/oocl/GuessNumberGameTest.java
+++ src/test/java/com/oocl/GuessNumberGameTest.java
@@ -1,8 +1,12 @@
 package com.oocl;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,7 +18,14 @@ public class GuessNumberGameTest {
         Boolean result = checkDuplicatedChar(randomAnswer);
         assertEquals(true, result);
     }
-
+    @Test
+    public void should_get_same_number_between_input_and_random_number(){
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        guessNumberGame.setRandomAnswer("5234");
+        List<Character> result = guessNumberGame.getCommonElement("1543");
+        List<Character> expect = Arrays.asList('5','4','3');
+        Assert.assertEquals(expect,result);
+    }
     private Boolean checkDuplicatedChar(String randomAnswer) {
         HashSet<Character> characterSet = new HashSet<Character>();
         for (int index = 0; index < randomAnswer.length(); index++) {
