29d19babc52d0989db4cba8790e5572bb7abe009
JAY NGAN (DEV-ISD-OOCLL/HKG) <NGANJA@OOCL.COM>
2020-04-08 21:13:49 +0800

    [NGANJA] add  Console UI

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,10 +1,26 @@
 package com.oocl;
 
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
+    public static final int MAX_CHANCE = 6;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Start Game! Please gues:\n");
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        int chance=0;
+        while(chance<MAX_CHANCE) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.next();
+            String result = guessNumberGame.play(input);
+            if(result.equals("4A0B")){
+                System.out.println(result);
+                break;
+            }
+            System.out.println(result);
+            chance++;
+        }
     }
 }
